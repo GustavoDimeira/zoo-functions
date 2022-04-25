b244af5700df1fdc1f032ed9213a83f4062c9a99
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-04-25 10:28:48 -0400

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const array = data.employees.filter((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+  if (array[0] === undefined) {
+    return {};
+  }
+  return array[0];
 }
 
 module.exports = getEmployeeByName;
+
+const array = [{1: 1}];
+
+console.log(array[0]);
