0656634580c939530ee0b1a0aa5f545feee94442
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-04-25 10:13:04 -0400

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+function getAnimalsOlderThan(animalPassado, age) {
+  const animal = data.species.find((animal) => animal.name === animalPassado);
+  return animal.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+function getSpeciesByIds(...ids) {
+  }
 
 module.exports = getSpeciesByIds;
