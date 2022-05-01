eaa5a167ace8b0b7c70ed3f0ab0bd2d9f4143756
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-05-01 17:13:10 -0400

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
-//  const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-}
+  const aanimais = [];
+
+  ids.forEach((id) =>
+    {
+      const animais = data.species;
+      const animalCerto = animais.find((animal) => animal.id === id);
+      aanimais.push(animalCerto)
+    }
+  )
+  return aanimais;
+};
 
 module.exports = getSpeciesByIds;
