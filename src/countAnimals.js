43e7afb090a4772fec45144d6faef095415b5232
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-05-01 19:24:44 -0400

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    let objFinal;
+    data.species.forEach((specie1) => {
+      const nome = specie1.name;
+      const quantia = specie1.residents.length;
+      objFinal = {
+        ...objFinal,
+        [nome]: quantia,
+      }
+    })
+    return objFinal;
+  } else if (animal.specie !== undefined && animal.sex !== undefined) {
+    const { specie, sex } = animal;
+    if (sex === 'female') {
+      const oAnimal = data.species.find((animal) => animal.name === specie);
+      const femeas = oAnimal.residents.filter((animal) => animal.sex === 'female');
+      return femeas.length;
+    } else if (sex === 'male') {
+      const oAnimal = data.species.find((animal) => animal.name === specie);
+      const machos = oAnimal.residents.filter((animal) => animal.sex === 'male');
+      return machos.length;
+    }
+  } const { specie } = animal;
+    const oAnimal = data.species.find((animal) => animal.name === specie);
+    return oAnimal.residents.length;   
 }
 
 module.exports = countAnimals;
