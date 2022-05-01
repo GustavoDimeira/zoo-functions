ab8e12d1e65f4d137a7c063a865fc1ea240db8e3
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-05-01 19:56:55 -0400

    removendo complexidade

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,33 +1,31 @@
 const data = require('../data/zoo_data');
 
 let objFinal;
-const semNada = data.species.forEach((specie1) => {
+data.species.forEach((specie1) => {
   const nome = specie1.name;
   const quantia = specie1.residents.length;
   objFinal = {
     ...objFinal,
     [nome]: quantia,
   };
-  return objFinal;
 });
 
 function countAnimals(animal) {
   if (animal === undefined) {
     return objFinal;
-  } if (animal.specie !== undefined && animal.sex !== undefined) {
-    const { specie, sex } = animal;
+  } 
+  const { specie, sex } = animal;
+  const oAnimal = data.species.find((aanimal) => aanimal.name === specie);
+  if (animal.specie !== undefined && animal.sex !== undefined) {
     if (sex === 'female') {
-      const oAnimal = data.species.find((aanimal) => aanimal.name === specie);
       const femeas = oAnimal.residents.filter((aanimal) => aanimal.sex === 'female');
       return femeas.length;
     } if (sex === 'male') {
-      const oAnimal = data.species.find((aanimal) => aanimal.name === specie);
       const machos = oAnimal.residents.filter((aanimal) => aanimal.sex === 'male');
       return machos.length;
     }
-  } const { specie } = animal;
-  const oAnimal = data.species.find((aanimal) => aanimal.name === specie);
+  }
   return oAnimal.residents.length;
-}
+};
 
 module.exports = countAnimals;
