0999d410e0cf97ec8230206ee4cde7094047a2ee
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-05-01 17:22:13 -0400

    errinhos

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,14 +3,13 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...ids) {
   const aanimais = [];
 
-  ids.forEach((id) =>
-    {
-      const animais = data.species;
-      const animalCerto = animais.find((animal) => animal.id === id);
-      aanimais.push(animalCerto)
-    }
-  )
+  ids.forEach((id) => {
+    const animais = data.species;
+    const animalCerto = animais.find((animal) => animal.id === id);
+    aanimais.push(animalCerto);
+  }
+  );
   return aanimais;
-};
+}
 
 module.exports = getSpeciesByIds;
