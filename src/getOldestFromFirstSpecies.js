6fb9aa34e1252efc10917e9148e0d42577d8a23c
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-05-02 12:24:50 -0400

    errinhos

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 function getOldestFromFirstSpecies(id) {
   const specieCuidada = data.employees.find((employee) => employee.id === id).responsibleFor[0];
   const animais = data.species.find((specie) => specie.id === specieCuidada);
-  const maisVleho = animais.residents.reduce((acc, animal) => acc.age < animal.age ? animal : acc)
+  const maisVleho = animais.residents.reduce((acc, animal) => acc.age > animal.age ? acc : animal);
   const { name, sex, age } = maisVleho;
   return [name, sex, age];
 }
