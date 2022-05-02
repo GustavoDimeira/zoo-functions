a11689f7c741109eb319a031595072e09afbbae6
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-05-02 12:32:31 -0400

    if else,  para arrumar errinhos

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,7 +3,11 @@ const data = require('../data/zoo_data');
 function getOldestFromFirstSpecies(id) {
   const specieCuidada = data.employees.find((employee) => employee.id === id).responsibleFor[0];
   const animais = data.species.find((specie) => specie.id === specieCuidada);
-  const maisVleho = animais.residents.reduce((acc, animal) => acc.age > animal.age ? acc : animal);
+  const maisVleho = animais.residents.reduce((acc, animal) => {
+    if (acc.age > animal.age) {
+      return acc
+    } return animal
+  });
   const { name, sex, age } = maisVleho;
   return [name, sex, age];
 }
