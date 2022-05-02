ee084f35b3f2488314a12977b8c18dae7bfdba3a
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-05-02 12:35:37 -0400

    ponto e virgula

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -5,8 +5,8 @@ function getOldestFromFirstSpecies(id) {
   const animais = data.species.find((specie) => specie.id === specieCuidada);
   const maisVleho = animais.residents.reduce((acc, animal) => {
     if (acc.age > animal.age) {
-      return acc
-    } return animal
+      return acc;
+    } return animal;
   });
   const { name, sex, age } = maisVleho;
   return [name, sex, age];
