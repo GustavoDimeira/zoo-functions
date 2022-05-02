9925183244c002e2f9bc3f5e9dee376ac477eabf
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-05-02 09:59:37 -0400

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,31 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  entrants.forEach((pessoa) => {
+    if (pessoa.age < 18) {
+      child += 1
+    } else if (pessoa.age < 50) {
+      adult += 1
+     } else {
+       senior += 1;
+     }
+  });
+  const entradas = { child, adult, senior };
+  return entradas;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const child = countEntrants(entrants).child * data.prices.child;
+  const adult = countEntrants(entrants).adult * data.prices.adult;
+  const senior = countEntrants(entrants).senior * data.prices.senior;
+  const Total = child + adult + senior;
+  return Total;
 }
 
 module.exports = { calculateEntry, countEntrants };
