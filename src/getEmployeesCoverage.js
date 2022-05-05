10c1c258bfb3b2e510fde1545d8c9f14bcec4af2
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-05-04 21:22:00 -0400

    resolvendo lint

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,26 +1,7 @@
 const data = require('../data/zoo_data');
 
-/*
-Se o objeto de opções tiver a propriedade name, retorna somente a pessoa correspondente; feito
-A propriedade name do objeto de opções também funciona usando o segundo nome; feito
-Se o objeto de opções tiver a propriedade id, retorna somente a pessoa correspondente; 
-Sem parâmetros, retorna uma lista com a cobertura de todas as pessoas funcionárias;
-Caso não haja nenhuma pessoa com o nome ou id especificados deverá ser lançado um error.
-*/
-
-const expected = {
-  id: '4b40a139-d4dc-4f09-822d-ec25e819a5ad',
-  fullName: 'Sharonda Spry',
-  species: ['otters', 'frogs'],
-  locations: ['SE', 'SW'],
-};
-
-//ver os ids dos animais usando o foreach no respnsiblefor, e com isso pegar o nome e as localizações e colocar cada um em um array
-//usar um if para cada localização, se ela estiver no array de localizações, adicionar a variavel das localizações final, dessa forma a localização não se repete
-
 let animalNome = [];
 let localizacao = [];
-//usar o spread e lembrar de resetar as variaeis
 const speciesElocations = (objPessoa) => {
   animalNome = [];
   localizacao = [];
@@ -32,27 +13,23 @@ const speciesElocations = (objPessoa) => {
 };
 const criarObj = (objPessoa) => {
   speciesElocations(objPessoa);
-  const final =
-  {
+  const final = {
     id: objPessoa.id,
     fullName: `${objPessoa.firstName} ${objPessoa.lastName}`,
     species: animalNome,
     locations: localizacao,
   };
   return final;
-}
+};
 const porNome = (name) => {
-  //procura o nome e passa um obj pesssoa find
   const objPessoa = data.employees.find((pessoa) => pessoa.firstName === name);
   if (objPessoa !== undefined) {
     return criarObj(objPessoa);
   }
-  //procura o sobrenome passa obj pessoa find
   const objPessoa2 = data.employees.find((pessoa) => pessoa.lastName === name);
   if (objPessoa2 !== undefined) {
     return criarObj(objPessoa2);
   }
-  //mensagem de erro
   if (objPessoa === undefined && objPessoa2 === undefined) {
     throw new Error('Informações inválidas');
   }
@@ -66,28 +43,20 @@ const porId = (id) => {
   }
 };
 const todos = () => {
-  //cria um com todos map
-  let todos = []
+  let todosEmpregados = [];
   data.employees.forEach((pessoa) => {
-    todos = [...todos, criarObj(pessoa)];
+    todosEmpregados = [...todosEmpregados, criarObj(pessoa)];
   });
-  return todos;
+  return todosEmpregados;
 };
 function getEmployeesCoverage(options) {
   if (options === undefined) {
-    //pegar todos
     return todos();
   } if (options.name !== undefined) {
     return porNome(options.name);
-    //procuar nome e sobrenome feito
-    //lançar erro se n achcar feito
   } if (options.id !== undefined) {
     return porId(options.id);
-    //pegar pessoa por id
-    //lançar erro se n achar feito
   }
-};
+}
 
 module.exports = getEmployeesCoverage;
-
-console.log(getEmployeesCoverage());
